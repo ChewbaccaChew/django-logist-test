cb5893c8bc2771f3897cb3836e30cf6bef229b57
ChewbaccaChew <bobbydjunk@list.ru>
2022-10-16 00:05:19 +0300

    added class Carriers(models.Model) basic settings in settings.py

--- main/carriers/admin.py
+++ main/carriers/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from carriers.models import Carriers
 
-# Register your models here.
+
+# admin.site.register(Carriers)
+@admin.register(Carriers)
+class CarriersAdmin(admin.ModelAdmin):
+    list_display = ['organisation', 'op_form']
+    prepopulated_fields = {'slug': ('organisation',)}
--- main/carriers/models.py
+++ main/carriers/models.py
@@ -5,12 +5,15 @@ from django.contrib.auth.models import User
 from datetime import date
 
 
-class Genre(models.Model):
-    name = models.CharField(
-        max_length=200,
-        help_text='',
-        verbose_name=''
-    )
+class Carriers(models.Model):
+    organisation = models.CharField(max_length=200, help_text='Введите название организации', verbose_name='Название')
+    form = [('ИПБОЮЛ', 'IPBUL'), ('ООО', 'ooo'), ('ОАО', 'oao'), ('3АО', 'zao'), ('ФИЗЛИЦО', 'fizik')]
+    op_form = models.CharField(max_length=10, choices=form)
+    # models.BooleanField(default=True, verbose_name='Наличие слота для карты SD')
+    slug = models.SlugField(max_length=50)
+
+    def is_upperclass(self):
+        return self.op_form in {self.form[0][0], self.form[4][1]}
 
     def __str__(self):
-        return self.name
+        return self.organisation
